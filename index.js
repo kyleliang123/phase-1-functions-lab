997921e70b8377446ea22edd2e9e147d5a85ffd9
Kyle <kyleliang586@gmail.com>
2022-10-12 09:14:31 -0700

    commit tree 6062d7e05854e9cc234acf0f428c4e22e88eee3b

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+//problem set #1
+const feetPerBlock = 264
+const numBlocks = 42
+function distanceFromHqInBlocks(distance) {
+    let total
+    if (distance >= 42) {
+        total = `${distance}` - 42
+    } else {
+        total = 42 - `${distance}`
+    }
+    return total
+}
+distanceFromHqInBlocks(distance)
+
+//problem set #2
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * feetPerBlock
+}
+distanceFromHqInFeet(distance)
+
+//problem set #3
+function distanceTravelledInFeet(a, b) {
+    if (a >= b){
+        return (a - b) * feetPerBlock
+    } else {
+        return (b - a) * feetPerBlock
+    }
+}
+distanceTravelledInFeet(a, b)
+
+//problem set #4
+function calculatesFarePrice(c, d) {
+    let distance = distanceTravelledInFeet(c, d)
+    if (distance <= 400) {
+        return 0
+    } else if (distance >= 400 && distance <= 2000) {
+        return ((distance - 400)* 0.02)
+    } else if (distance > 2000 && distance < 2500)
+        return 25
+      else (distance > 2500)
+    return 'cannot travel that far'
+}
